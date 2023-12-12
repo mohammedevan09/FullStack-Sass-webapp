7495bdb16e6900e2f5ec12dd01fe20b840a85032
Mohammed Evan <121313913+mohammedevan09@users.noreply.github.com>
2023-12-12 16:25:02 +0600

    first commit

--- user-dashboard/app/page.js
+++ user-dashboard/app/page.js
@@ -1,11 +1,10 @@
-import Header from '@/components/Header'
-import Main from '@/components/Main'
-
 export default function Home() {
   return (
-    <div className="grid">
-      <Header />
-      <Main />
+    <div className="grid justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold">
+        {' '}
+        Here Will be Landing page of WP Sprint LLC
+      </h1>
     </div>
   )
 }
