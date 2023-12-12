b847a72854b2d26180becd87036bc7f7748bb122
Mohammed Evan <121313913+mohammedevan09@users.noreply.github.com>
2023-12-12 16:22:19 +0600

    first commit

--- user-dashboard/app/(pages)/dashboard/layout.jsx
+++ user-dashboard/app/(pages)/dashboard/layout.jsx
@@ -3,17 +3,23 @@ import Header from '@/components/Header'
 
 const layout = ({ children }) => {
   return (
-    <div className="relative mx-auto">
-      <div className="flex justify-center items-start gap-24">
-        <div className="w-[260px] text-center h-screen">
-          <Menu />
-        </div>
-        <div className="grid w-full justify-center">
-          <Header />
-          {children}
+    <>
+      <div className="bg-blue-800 w-full text-center text-white text-xl font-semibold leading-tight py-5 sticky top-0 z-[9999999]">
+        10% flat discount for any project. From 25 September to October 25!
+        COUPON : #CRAZY10
+      </div>
+      <div className="relative mx-auto">
+        <div className="flex justify-center items-start gap-24">
+          <div className="w-[260px] text-center h-screen">
+            <Menu />
+          </div>
+          <div className="grid w-full justify-center">
+            <Header />
+            {children}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
