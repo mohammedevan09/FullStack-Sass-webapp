299e146c598fb4e92e85ed0de3af7d05cecdb64a
Mohammed Evan <121313913+mohammedevan09@users.noreply.github.com>
2024-05-21 11:56:53 +0600

    updated

--- user-dashboard/app/(pages)/dashboard/settings/layout.jsx
+++ user-dashboard/app/(pages)/dashboard/settings/layout.jsx
@@ -1,38 +1,7 @@
-'use client'
-
-import { useSelector } from 'react-redux'
-import MainSettingsLayoutPage from './MainSettingsLayoutPage'
+import SubLayout from './SubLayout'
 
 const layout = ({ children }) => {
-  const { userInfo } = useSelector((state) => state?.user)
-
-  const allSettings = [
-    {
-      title: 'Profile',
-      link: '/dashboard/settings',
-      comp: '',
-    },
-    ...(!userInfo?.creatorId
-      ? [
-          {
-            title: 'Notifications',
-            link: '/dashboard/settings/notifications',
-            comp: '',
-          },
-        ]
-      : []),
-  ]
-
-  return (
-    <div className="grid w-full">
-      <div className="text-gray-800 text-2xl font-bold md:mt-20 sm:mt-10 mt-6 mb-8 sm:px-4 xs:px-3 px-1">
-        Settings
-      </div>
-
-      <MainSettingsLayoutPage allSettings={allSettings} />
-      <div className="sm:px-4 xs:px-3 px-1">{children}</div>
-    </div>
-  )
+  return <SubLayout>{children}</SubLayout>
 }
 
 export default layout
